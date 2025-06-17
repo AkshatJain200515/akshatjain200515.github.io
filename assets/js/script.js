be491b5d94f9279eb18aa37b371809fb4c8da5d6
AkshatJain200515 <Akshatjain200515@gmail.com>
2025-06-17 07:39:49 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["python developer", "sql developer", "Tech Problem Solving", "Creative Thinking", "Building Solutions That Matter", "Always Learning, Always Growing"],
+    strings: ["Python developer", "SQL developer", "Tech Problem Solving", "Creative Thinking", "Building Solutions That Matter", "Always Learning, Always Growing"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
